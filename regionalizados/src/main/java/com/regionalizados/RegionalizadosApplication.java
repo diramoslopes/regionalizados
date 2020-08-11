c04da4dd66666522f7faac7cf56f18088d0d91f4
Diogo <dramoslopesilveira@gmail.com>
2020-08-11 14:19:08 -0300

    iniciando regionalizados

---
+++ regionalizados/src/main/java/com/regionalizados/RegionalizadosApplication.java
@@ -0,0 +1,13 @@
+package com.regionalizados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionalizadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionalizadosApplication.class, args);
+	}
+
+}
---
+++ regionalizados/src/test/java/com/regionalizados/RegionalizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regionalizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionalizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
